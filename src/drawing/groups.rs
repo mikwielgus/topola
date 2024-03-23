6a5be3c368879ccc5817752d65ad0166e1013a4c
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2024-03-23 18:50:00 +0000

    dsn,drawing: remove mistakenly committed incomplete files

--- src/drawing/groups.rs
+++
@@ -1,8 +0,0 @@
-use enum_dispatch::enum_dispatch;
-
-use super::graph::GeometryIndex;
-
-#[enum_dispatch]
-pub trait GetGroups< {
-    fn node_groups(&self, node: GeometryIndex) -> Vec<GeometryIndex>;
-}
