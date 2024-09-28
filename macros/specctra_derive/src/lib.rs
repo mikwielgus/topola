d86ef12e831f48891e3de274e7c52fe929323675
Tomasz Cichoń <tcichon98@gmail.com>
2024-09-28 23:59:15 +0200

    specctra_derive: fix mistaken declarations of macro attributes

--- macros/specctra_derive/src/lib.rs
+++ macros/specctra_derive/src/lib.rs
@@ -4,13 +4,13 @@ use syn::{Attribute, DeriveInput, LitStr};
 mod read;
 mod write;
 
-#[proc_macro_derive(ReadDsn, attributes(opt, anon, vec, anon_vec))]
+#[proc_macro_derive(ReadDsn, attributes(anon, vec, anon_vec))]
 pub fn derive_read(input: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(input as DeriveInput);
     read::impl_read(&input).into()
 }
 
-#[proc_macro_derive(WriteSes, attributes(anon))]
+#[proc_macro_derive(WriteSes, attributes(anon, vec, anon_vec))]
 pub fn derive_write(input: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(input as DeriveInput);
     write::impl_write(&input).into()
@@ -27,5 +27,9 @@ fn attr_content(attrs: &Vec<Attribute>, name: &str) -> Option<String> {
     attrs
         .iter()
         .find(|attr| attr.path().is_ident(name))
-        .and_then(|attr| Some(attr.parse_args::<LitStr>().expect("string literal").value()))
+        .and_then(|attr| Some(attr
+            .parse_args::<LitStr>()
+            .expect("string literal")
+            .value()
+        ))
 }
