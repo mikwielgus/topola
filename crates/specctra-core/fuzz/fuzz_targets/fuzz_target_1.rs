0b37bb2dbcc743a5e83ccd35100a161ef54d2e75
Alain Emilia Anna Zscheile <fogti+devel@ytrizja.de>
2024-12-05 19:51:55 +0100

    feat: add rudimentary fuzzer for specctra-core DSN parser
    Fixes #110

---
+++ crates/specctra-core/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -0,0 +1,13 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: &str| {
+    let mut cursor = std::io::Cursor::new(data);
+
+    use specctra_core::{read::ListTokenizer, structure::DsnFile};
+
+    let mut tkz = ListTokenizer::new(cursor);
+
+    let _: Result<_, _> = tkz.read_value::<DsnFile>();
+});
