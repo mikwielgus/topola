758677970a4afae112923c21b015dfd1c3d36f46
Ellen Emilia Anna Zscheile <fogti+devel@ytrizja.de>
2025-02-10 11:13:55 +0100

    chore: allow unused_must_use (we didn't yet get around to fix that)

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,22 @@
-#![doc(
-
 // SPDX-FileCopyrightText: 2024 Topola contributors
 //
 // SPDX-License-Identifier: MIT
-
-html_favicon_url = "https://codeberg.org/topola/topola/raw/commit/e1b56875edf039aab9f41868826bcd3a92097133/assets/favicon.ico"
-)]
-#![doc(
-    html_logo_url = "https://codeberg.org/topola/topola/raw/commit/e1b56875edf039aab9f41868826bcd3a92097133/assets/logo.svg"
-)]
-#![cfg_attr(not(feature = "disable_contracts"), feature(try_blocks))]
 //! [Topola](https://topola.dev) is a work-in-progress interactive
 //! topological router in Rust.
 //!
 //! The project is funded by the [NLnet Foundation](https://nlnet.nl/) from
 //! the [NGI0 Entrust](https://nlnet.nl/entrust/) fund.
+
+#![doc(
+    html_favicon_url = "https://codeberg.org/topola/topola/raw/commit/e1b56875edf039aab9f41868826bcd3a92097133/assets/favicon.ico"
+)]
+#![doc(
+    html_logo_url = "https://codeberg.org/topola/topola/raw/commit/e1b56875edf039aab9f41868826bcd3a92097133/assets/logo.svg"
+)]
+#![cfg_attr(not(feature = "disable_contracts"), feature(try_blocks))]
+
+// TODO: fix all occurences
+#![allow(unused_must_use)]
 
 pub mod graph;
 #[macro_use]
