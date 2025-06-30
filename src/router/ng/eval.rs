e3790eb3663fdb052f3a4fd47c87749ce3981199
Ellen Emilia Anna Zscheile <fogti+devel@ytrizja.de>
2025-06-30 15:09:26 +0200

    fix(router/ng/eval): Flip alignment of outer edges
    This makes it possible to route some paths, altho it looks really wrong on first glace.
    I don't yet know exactly what's going on.

--- src/router/ng/eval.rs
+++ src/router/ng/eval.rs
@@ -191,8 +191,8 @@ impl AstarContext {
                         }
                         alignment
                     }
-                    (Some(_), None) => Alignment::Left,
-                    (None, Some(_)) => Alignment::Right,
+                    (Some(_), None) => Alignment::Right,
+                    (None, Some(_)) => Alignment::Left,
                     // this should only happen when one end-point is primal, handled above
                     (None, None) => unreachable!(),
                 };
