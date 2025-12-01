3b56eb74c973b148350fc1b0ebcb2040548cbdbc
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2025-12-01 01:32:46 +0100

    fix(geometry/primitive): Fix constants altered during debugging

--- src/geometry/primitive.rs
+++ src/geometry/primitive.rs
@@ -294,8 +294,8 @@ impl BendShape {
     /// Determines if the provided point lies in the swept region between the rays cast by tangents
     /// to the arc endpoints of the bend's outer circle, starting at `from` and `to`.
     pub fn between_tangent_rays(&self, point: Point) -> bool {
-        math::perp_dot_product(point - self.from, self.from_tangent_ray()) > 0.001
-            && math::perp_dot_product(point - self.to, self.to_tangent_ray()) < -0.001
+        math::perp_dot_product(point - self.from, self.from_tangent_ray()) > 0.0
+            && math::perp_dot_product(point - self.to, self.to_tangent_ray()) < 0.0
     }
 
     pub fn from_tangent_ray(&self) -> Point {
