299afde271287cc79346746f6bd237d0c355f3ba
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2026-06-03 19:44:12 +0200

    Update to undoredo version 0.11

--- topola/src/board/interactors/drag_move.rs
+++ topola/src/board/interactors/drag_move.rs
@@ -4,7 +4,7 @@
 
 use derive_getters::Getters;
 use derive_more::Constructor;
-use undoredo::DiscardDelta;
+use undoredo::ResetDelta;
 
 use crate::{Board, LayerId, Vector2, selections::ComponentSelection};
 
@@ -17,13 +17,13 @@ pub struct DragMoveInteractor {
 
 impl DragMoveInteractor {
     pub fn hold(&mut self, board: &mut Board, pointer: Vector2<i64>) {
-        board.discard_delta();
+        board.reset_delta();
 
         board.move_components_by(self.selection.clone(), pointer - self.origin);
     }
 
     pub fn abort(&mut self, board: &mut Board) {
-        board.discard_delta();
+        board.reset_delta();
     }
 
     pub fn release(&mut self, board: &mut Board, pointer: Vector2<i64>) {
